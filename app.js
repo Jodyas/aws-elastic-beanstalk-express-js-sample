1a2e19e9b924f927428c4c666a2603c999d77bbe
Jodyas <jodyas1993@gmail.com>
2021-05-16 21:44:37 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Manual CICD'));
+app.get('/', (req, res) => res.send('Automated CICD'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
